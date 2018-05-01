fc311f22073f6b5e64beeea4b731c6a99e76b58d
Adam Reevesman <areevesman@Adams-MacBook-Pro.local>
2018-05-01 16:39:37 -0700

    improved shiny app

--- shiny/compare_teams/compare_teams_app.R
+++ shiny/compare_teams/compare_teams_app.R
@@ -8,32 +8,74 @@
 #
 
 library(shiny)
+library(shinythemes)
 library(RCurl)
 library(plyr)
 library(dplyr)
 library(ggplot2)
 
 
+### Season Data
 
+
+#read in all of the data
 d17_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/dodgers2017.csv")
 a17_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/astros2017.csv")
+c16_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/cubs2016.csv")
+i16_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/indians2016.csv")
+r15_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/royals2015.csv")
+m15_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/mets2015.csv")
+g14_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/giants2014.csv")
+r14_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/royals2014.csv")
+c13_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/cardinals2013.csv")
+r13_URL <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/redsox2013.csv")
 
-d17 <- read.csv(text = d17_URL) %>%
-  as_tibble() %>%
-  mutate(wins_so_far = cumsum(!grepl(x=W.L,pattern='L')),
-         runs_so_far = cumsum(R),
-         RA_so_far = cumsum(RA),
-         streak = Streak)
+
+all_urls <- c(d17_URL,a17_URL,
+              c16_URL,i16_URL,
+              r15_URL,m15_URL,
+              g14_URL,r14_URL,
+              c13_URL,r13_URL)
+
+#store all 10 dataframes in a list
+all_data_frames <- list()
+for (i in 1:length(all_urls)){
+  
+  if (i %in% 1:2){
+    year <- 2017
+  }
+  else if (i %in% 3:4){
+    year <- 2016
+  }
+  else if (i %in% 5:6){
+    year <- 2015
+  }
+  else if (i %in% 7:8){
+    year <- 2014
+  }
+  else {
+    year <- 2013
+  }
+  
+  all_data_frames[[i]] <- read.csv(text = all_urls[i], 
+                                   stringsAsFactors = FALSE) %>%
+    as_tibble() %>%
+    mutate(wins_so_far = cumsum(!grepl(x=W.L,pattern='L')),
+           runs_so_far = cumsum(R),
+           RA_so_far = cumsum(RA),
+           year = year)
+  
+}
+names(all_data_frames) <- c('d17','a17',
+                            'c16','i16',
+                            'r15','m15',
+                            'g14','r14',
+                            'c13','r13')
 
 
-a17 <- read.csv(text = a17_URL) %>%
-  as_tibble() %>%
-  mutate(wins_so_far = cumsum(!grepl(x=W.L,pattern='L')),
-         runs_so_far = cumsum(R),
-         RA_so_far = cumsum(RA),
-         streak = Streak)
 
-combined_init <- bind_rows(d17,a17) 
+#combine all data frames
+combined_init <- bind_rows(all_data_frames) 
 
 combined <- tibble(team = combined_init$Tm, 
                    game_num = combined_init$Gm., 
@@ -45,7 +87,7 @@ combined <- tibble(team = combined_init$Tm,
                    wins_so_far = combined_init$wins_so_far,
                    runs_so_far = combined_init$runs_so_far,
                    RA_so_far = combined_init$RA_so_far,
-                   streak = combined_init$streak) %>%
+                   year = combined_init$year) %>%
   mutate(games_ahead = gsub(x = games_ahead,
                             pattern = 'Tied',
                             replacement = '0')) %>%
@@ -57,48 +99,59 @@ combined <- tibble(team = combined_init$Tm,
                             x = games_ahead)) %>%
   mutate(games_ahead = -1*as.numeric(games_ahead)) 
 
-attach(combined)
+
+
+### Columns spelled out
+
+labels_df <- data.frame(abrv = c('game_num',
+                              'wins_so_far',
+                              'runs_so_far',
+                              'RA_so_far',
+                              'games_ahead'),
+                     spelled_out = c("Games Played", 
+                                     "Number of Wins", 
+                                     "Number of Runs Scored",
+                                     "Number of Runs Allowed", 
+                                     "Number of Games Ahead"),
+                     stringsAsFactors = FALSE)
+
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
+ui <- fluidPage(theme = shinytheme("superhero"),
 
      
    # Application title
    titlePanel("Compare Opposing World Series Teams"),
    
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-        plotOutput("distPlot")
-      )
-      
-   ),
    
    
-   # Sidebar with a checkbox with boxes for statitsic
+   #Sidebar with
+   #a select box for year
+   #a checkbox with boxes for statitsic
+   #a slider range for range of games
    sidebarLayout(
      sidebarPanel(
+       
+       #make a select box 
+       selectInput("select", label = h3("Year"), 
+                   choices = list("2017" = 2017, "2016" = 2016, "2015" = 2015,
+                                  "2014" = 2014, "2013" = 2013), 
+                   selected = 2017),
+       
        radioButtons("radio", label = h3("Statistic"), 
                           choices = list("Wins per Games Played" = 'wins_so_far', 
                                          "Runs per Game" = 'runs_so_far',
                                          "Runs Allowed Per Game" = 'RA_so_far',
                                          "Games Ahead in Division" = 'games_ahead'),
-                          selected = 'wins_so_far')
+                          selected = 'wins_so_far'),
+       
+       sliderInput("slider", label = h3("Range of Regular Season Games"), min = 0, 
+                   max = 162, value = c(0,162))
      ),
      
      # Show a plot of the generated statistic
      mainPanel(
-       plotOutput("statPlot"),
-       plotOutput("statPlot2")
+       plotOutput("statPlot")
      )
      
    )
@@ -111,28 +164,26 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
-   
-   
-   
-   # You can access the values of the widget (as a vector)
-   # with input$checkGroup, e.g.
-   output$statPlot <- renderPlot({
-     
-     combined %>% 
-       ggplot(aes(x = game_num, y = combined[,input$radio[1]], color = team)) +
-       geom_line(size = 1.5)
-      
-     
-     
-     })
+  
+  combined_year <- reactive({
+    a <- combined %>% filter(year == input$select)
+    return(a)
+  })
+  
+  output$statPlot <- renderPlot({
+    
+    combined_year() %>% 
+      ggplot(aes(x = game_num, y = combined_year()[,input$radio], color = team)) +
+      geom_line(size = 1.5) +
+      xlim(input$slider) +
+      xlab((labels_df %>% filter(abrv == 'game_num') %>% select(spelled_out))[1,1]) +
+      ylab((labels_df %>% filter(abrv == input$radio) %>% select(spelled_out))[1,1]) +
+      ggtitle(paste("Total Number of", 
+                    ylab((labels_df %>% filter(abrv == input$radio) %>% select(spelled_out))[1,1])$y,
+                    "by Number of", 
+                    xlab((labels_df %>% filter(abrv == 'game_num') %>% select(spelled_out))[1,1])$x))
+    
+  })
 
    
    
