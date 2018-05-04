6e6ecca982e4b0eaff4e0660822e9bfafc3d9ec8
Adam Reevesman <areevesman@Adams-MacBook-Pro.local>
2018-05-03 21:43:42 -0700

    added web scraper/other things

--- scripts/age.R
+++ scripts/age.R
@@ -5,18 +5,31 @@ library(ggplot2)
 
 
 #read in all of the data
-#standings_url <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/standings2017.csv")
+standings_url <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/standings2017.csv")
 batting_url <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/batting2017.csv")
-pitching_url <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/pitching2016.csv")
+pitching_url <- getURL("https://raw.githubusercontent.com/areevesman/world_series/master/data/pitching2017.csv")
 
 
+
+standings <- read.csv(text = standings_url, 
+                     stringsAsFactors = FALSE)[,-1] %>%
+  as_tibble() %>% 
+  filter(!(Tm  %in% c('Avg','LgAvg','')))%>%
+  arrange(Tm)
 
 batting <- read.csv(text = batting_url, 
             stringsAsFactors = FALSE) %>%
-  as_tibble()
+  as_tibble() %>% 
+  filter(!(Tm  %in% c('Avg','LgAvg','')))%>%
+  arrange(Tm)
+
+pitching <- read.csv(text = pitching_url, 
+                    stringsAsFactors = FALSE) %>%
+  as_tibble() %>% 
+  filter(!(Tm  %in% c('Avg','LgAvg','')))%>%
+  arrange(Tm)
 
 
-
-
+plot(x = batting$SLG*batting$RBI, y = standings$W)
 
 
