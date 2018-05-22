1bd8b22858479f457319e25f35c9b81c04bcce3a
Adam Reevesman <areevesman@Adams-MacBook-Pro.local>
2018-05-22 16:24:47 -0700

    fix merge issues

--- shiny/compare_all_teams/app.R
+++ shiny/compare_all_teams/app.R
@@ -154,12 +154,7 @@ server <- function(input, output) {
       geom_point(size = 2) +
       geom_line(size = 1) +
       xlim(c(-5,165)) +
-<<<<<<< HEAD
       ylim(limit_data()) +
-=======
-      ylim(c(-q.q * abs(as.numeric(limit_data()[which(limit_data()$var == input$select_stat), "min_val"][[1,1]])),
-             1.1 * as.numeric(limit_data()[which(limit_data()$var == input$select_stat), "max_val"][[1,1]]))) +
->>>>>>> 1d1bf60d396c23bfb64e6af26d410625e79b1b24
       xlab("Game Number in Season") +
       ylab("")
     
